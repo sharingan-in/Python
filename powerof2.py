e693a19b5f22e8163881719c25e5e1aca01d6a95
Vaithee <svaithee12@gmail.com>
2015-10-31 23:07:43 +0530

    Update powerof2.py

--- powerof2.py
+++ powerof2.py
@@ -6,12 +6,12 @@ while t :
     j=1
     
     if m != 0 and ((m & (m - 1)) == 0) :
-        
         print('yes')
     	
         
     else :
         print('NO')
+        
     t=t-1
     
     
