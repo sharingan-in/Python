2d2cec3807654175cf60324b2ad05b3f806abd43
Shubham <shubhams805@gmail.com>
2015-11-03 20:29:18 +0530

    this program first lists the css file used on the page and then save them accordind to users instructions

---
+++ BeautifulSoup/copy-style.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+
+import requests
+
+url = input("Enter a  URL :  ")
+
+r  = requests.get("http://" +url)
+
+data = r.text
+
+i=-1
+
+choice=[]
+soup = BeautifulSoup(data)
+
+print("Stylesheets used on the page are : \n")
+for link in soup.find_all('link'):
+	str1 = link.get('rel')
+	if(str1==['stylesheet']) :
+		i=i+1
+		k=str(i+1)
+		print (k+" . " +link.get('href'))
+		choice.append(link.get('href'))
+
+m = i
+i=0
+n=int(input("enter the serial number of the css file to save ,or \n press 0 to save all files \n : "))
+if n==0 :
+	while i<m :
+		s=str(i+1)
+		r=requests.get(url +'/'+choice[i])
+		code=r.text
+		content=BeautifulSoup(code)
+		fl = open("css_file"+ s +".txt",'w+')
+		fl.write(str(content))
+		i=i+1
+		fl.close()
+else :
+	s=str(n)
+	fl= open("css_file"+ s + ".txt",'w+')
+	r=requests.get("http://" + url +'/' +choice[n-1])
+	code=r.text
+	content=BeautifulSoup(code)
+	fl.write(str(content))
+	fl.close()
