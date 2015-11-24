92887feca29614e3f86b78aa305ac3be74812436
Shubham <shubhams805@gmail.com>
2015-11-24 18:27:06 +0530

    trying to list top 5 products with there percentage

--- BeautifulSoup/jabong-scraping.py
+++ BeautifulSoup/jabong-scraping.py
@@ -1,12 +1,18 @@
 from bs4 import BeautifulSoup
+import operator
 import requests
 url = input("Enter the url")
-req = requests.get("http://" + url)
+if url[0:4]=="http":
+	req = requests.get(url)
+else :
+	req = requests.get("http://" + url)
 content = req.text
 code= BeautifulSoup(content)
 fl = open("file"+'2'+".txt",'w+')
 store = code.find('div',"all-brand-filters")
 i=0
+sum1=0
+count=0
 brand={}
 num=[]
 for i in store.find_all('label') :
@@ -22,9 +28,34 @@ for i in store.find_all('label') :
 		j=j+1
 	
 	num1=num[1:j]
-
+	sum1+=int(num1)
+	count+=1
 	
 	brand[brand_name]=num1
 fl.write(str(brand));
-print(brand)
+i=0
+n =str(input("enter the product name or enter 0 for all"))
+if n=='0':
+ 	for value in brand :
+ 		print(value + " has total of \t"+ brand[value] +" products on jabong")
+ 		i=i+1
+ 		
+else :
+	print(n +" has total of " + brand[n] +" products")
+	price =brand[n]
+	price=int(price)
+	k=int(price*100/sum1)
+	print("\n " + n + " contributes to approximately " + k + " percentage of products on jabong")
+print("\n\n\n top 5 brands on jabong for current category are :")
+sorted_brand = sorted(brand.items(), key=operator.itemgetter(1))
+i=0
+for value in sorted_brand :
+	price=value[i]
+	price=int(price)
+	k=int(price*100/sum1)
+	print(value + " has total of " + sorted_brand[value] +" products on jabong whis is about " +k + "'%' of all products")
+	i=i+1
+	if i>=5:
+		break
+
 fl.close()
