d41f0387d111a5c3d2b90540c6257825f666d156
SamWinchester <shubhams805@gmail.com>
2016-08-07 12:47:24 +0530

    removed incorrect scripts

--- BeautifulSoup/copy-code.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-url = input("Enter the url")
-req = requests.get("http://" + url)
-content = req.text
-code= BeautifulSoup(content)
-fl = open("file.txt",'w+')
-fl.write(str(code))
-fl.close()
